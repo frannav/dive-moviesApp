b41c823894c2529ac36bf5bd21d6266c3cbaa891
frannav <frannavmart@gmail.com>
2022-05-30 19:39:07 +0100

    Added layout

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import styles from "../styles/Nav.module.css";
+import type { AppProps } from "next/app";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <nav>
+        <Link href="/">
+          <a>MoviesApp</a>
+        </Link>
+      </nav>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
